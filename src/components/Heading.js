7464526716aa5337031e493b2610df495c15c34c
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2021-03-16 22:11:07 +0530

    weather-tracker

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Heading = (props) =>{
-    return(){
+    return(
         <div>
             <h1>Explore in World's Weather?</h1>
             <h3>Find out weather conditions are in your city.</h3>
         </div>
-    }
+    )
 }
+
+export default Heading;
